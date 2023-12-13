cd35f551914407e3c4722fc9daccb2c24d04868e
Dominik Honnef <dominik@honnef.co>
2023-12-13 15:13:56 +0100

    WIP mutex

--- cmd/gotraceui/main.go
+++ cmd/gotraceui/main.go
@@ -2065,3 +2065,33 @@ func cmp[T constraints.Ordered](a, b T, negate bool) int {
 	}
 	return ret
 }
+
+type Mutex[T any] struct {
+	mu sync.RWMutex
+	v  T
+}
+
+type MutexUnlock struct {
+	mu *sync.RWMutex
+}
+
+type MutexRUnlock struct {
+	mu *sync.RWMutex
+}
+
+func NewMutex[T any](v T) *Mutex[T] {
+	return &Mutex[T]{v: v}
+}
+
+func (mu *Mutex[T]) Lock() (T, MutexUnlock) {
+	mu.mu.Lock()
+	return mu.v, MutexUnlock{&mu.mu}
+}
+
+func (mu *Mutex[T]) RLock() (T, MutexRUnlock) {
+	mu.mu.RLock()
+	return mu.v, MutexRUnlock{&mu.mu}
+}
+
+func (u MutexUnlock) Unlock()   { u.mu.Unlock() }
+func (u MutexRUnlock) RUnlock() { u.mu.RUnlock() }
